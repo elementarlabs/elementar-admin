7fea18a5b06feffc94ae85ebb57930c356d59836
sefatanam <sefatanam@gmail.com>
2024-04-23 16:32:01 +0600

    refactor(alert): remove redundant matRipple usage
    In the Alert Actions component the MatRipple directive was added redundantly in the component. Since MatRipple is already imported and used as a directive, it need to be included in hostDirectives of AlertActionDirective. This commit removes the redundant usage, improving code clarity.

--- projects/components/src/lib/alert/alert-action.directive.ts
+++ projects/components/src/lib/alert/alert-action.directive.ts
@@ -1,11 +1,17 @@
 import { Directive } from '@angular/core';
+import { MatRipple } from "@angular/material/core";
 
 @Directive({
   selector: '[emrAlertAction]',
   exportAs: 'emrAlertAction',
   host: {
     'class': 'emr-alert-action'
-  }
+  },
+  hostDirectives: [
+    {
+      directive: MatRipple
+    }
+  ]
 })
 export class AlertActionDirective {
 }
--- src/app/pages/components/alert/_examples/alert-actions-example/alert-actions-example.component.ts
+++ src/app/pages/components/alert/_examples/alert-actions-example/alert-actions-example.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { EmrAlertModule } from '@elementar/components';
 import { MatIcon } from '@angular/material/icon';
-import { MatRipple } from '@angular/material/core';
 
 @Component({
   selector: 'app-alert-actions-example',
   standalone: true,
   imports: [
     EmrAlertModule,
-    MatIcon,
-    MatRipple
+    MatIcon
   ],
   templateUrl: './alert-actions-example.component.html',
   styleUrl: './alert-actions-example.component.scss'
