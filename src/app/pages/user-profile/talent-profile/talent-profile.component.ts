284631996258a4293ad2d395127152a965b64f52
Pavel Salauyou <pavel.salauyou@gmail.com>
2024-04-26 21:42:36 +0400

    changes on talent profile

--- src/app/pages/user-profile/talent-profile/talent-profile.component.ts
+++ src/app/pages/user-profile/talent-profile/talent-profile.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatAnchor, MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
-import { IconComponent } from '@elementar/components';
+import { EmrAvatarModule, IconComponent } from '@elementar/components';
+
+interface Person {
+  avatarUrl: string;
+  name: string;
+  occupation: string;
+}
 
 @Component({
   standalone: true,
@@ -11,13 +17,32 @@ import { IconComponent } from '@elementar/components';
     MatIcon,
     MatIconButton,
     MatTooltip,
-    IconComponent
+    IconComponent,
+    EmrAvatarModule,
+    MatAnchor
   ],
   templateUrl: './talent-profile.component.html',
   styleUrl: './talent-profile.component.scss'
 })
 export class TalentProfileComponent {
   followed = false;
+  peopleAlsoViewed: Person[] = [
+    {
+      avatarUrl: 'https://xsgames.co/randomusers/assets/avatars/female/44.jpg',
+      name: 'Beaulah Hansmann',
+      occupation: 'CEO of amazing company'
+    },
+    {
+      avatarUrl: 'https://xsgames.co/randomusers/assets/avatars/female/45.jpg',
+      name: 'Laree Alaman',
+      occupation: 'CEO of amazing company'
+    },
+    {
+      avatarUrl: 'https://xsgames.co/randomusers/assets/avatars/male/45.jpg',
+      name: 'Tom Hearron',
+      occupation: 'Software engineer'
+    }
+  ];
 
   follow(): void {
     this.followed = true;
