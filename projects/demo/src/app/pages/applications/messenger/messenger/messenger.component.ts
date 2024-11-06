16714b0df3f0767d110397cec0f07cb76a146290
Pavel Salauyou <pavel.salauyou@gmail.com>
2024-11-06 20:10:36 +0100

    add messenger component

--- projects/demo/src/app/pages/applications/messenger/messenger/messenger.component.ts
+++ projects/demo/src/app/pages/applications/messenger/messenger/messenger.component.ts
@@ -5,6 +5,19 @@ import { MatIconButton } from '@angular/material/button';
 import { DicebearComponent } from '@elementar/components/avatar';
 import { DatePipe } from '@angular/common';
 
+interface MessengerMessage {
+  id: any;
+  sender: {
+    avatarUrl: string;
+    name: string;
+    id: any;
+  };
+  title: string;
+  lastMessage: string;
+  createdAt: string | Date;
+  unreadMessagesCount: number;
+}
+
 @Component({
   selector: 'app-messenger',
   standalone: true,
@@ -19,16 +32,40 @@ import { DatePipe } from '@angular/common';
   styleUrl: './messenger.component.scss'
 })
 export class MessengerComponent {
-  messages = [
+  selectedMessageId = 1;
+  messages: MessengerMessage[] = [
     {
+      id: 1,
       sender: {
         avatarUrl: '',
         name: 'Alejandra Cubides',
         id: 1
       },
+      unreadMessagesCount: 2,
+      title: 'Looking for an Angular expert to upgrade angular project to the latest version',
+      lastMessage: 'Angular itself is easy to upgrade, most problems arise with third-party libraries and deprecated code.',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      sender: {
+        avatarUrl: '',
+        name: 'Pavel Salauyou',
+        id: 2
+      },
+      unreadMessagesCount: 0,
       title: 'Looking for an Angular expert to upgrade angular project to the latest version',
       lastMessage: 'Angular itself is easy to upgrade, most problems arise with third-party libraries and deprecated code.',
       createdAt: new Date(),
     }
   ];
+  sidebarActive = true;
+
+  selectMessage(message: MessengerMessage) {
+    this.selectedMessageId = message.id;
+  }
+
+  toggleSidebar() {
+    this.sidebarActive = !this.sidebarActive;
+  }
 }
