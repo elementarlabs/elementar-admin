c12ed585bd74c460e0b49047a4359e4571913715
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-01-16 18:34:00 +0100

    ref skeleton styles

--- projects/components/src/skeleton/skeleton/skeleton.component.ts
+++ projects/components/src/skeleton/skeleton/skeleton.component.ts
@@ -1,15 +1,15 @@
 import { Component, input } from '@angular/core';
 
 @Component({
-    selector: 'emr-skeleton',
-    exportAs: 'emrSkeleton',
-    templateUrl: './skeleton.component.html',
-    styleUrls: ['./skeleton.component.scss'],
-    host: {
-        'class': 'emr-skeleton',
-        '[class.is-direction-row]': 'direction() === "row"',
-        '[class.is-direction-col]': 'direction() === "col"',
-    }
+  selector: 'emr-skeleton',
+  exportAs: 'emrSkeleton',
+  templateUrl: './skeleton.component.html',
+  styleUrl: './skeleton.component.scss',
+  host: {
+    'class': 'emr-skeleton',
+    '[class.is-direction-row]': 'direction() === "row"',
+    '[class.is-direction-col]': 'direction() === "col"',
+  }
 })
 export class SkeletonComponent {
   direction = input<'row' | 'col'>('row');
