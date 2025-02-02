472a543fdab819ee4cff4922334c34ecd13635f2
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-02-02 15:57:10 +0100

    refactor breadcrumbs component styles to css vars

--- projects/components/preset.js
+++ projects/components/preset.js
@@ -193,6 +193,37 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries')
+    require('@tailwindcss/container-queries'),
+    require('tailwind-css-variables')(
+      {
+        colors: 'color',
+        screens: 'screen',
+        fontFamily: 'font',
+        fontSize: 'text',
+        fontWeight: 'font',
+        lineHeight: 'leading',
+        letterSpacing: 'tracking',
+        backgroundSize: 'bg',
+        borderWidth: 'border',
+        borderRadius: 'rounded',
+        width: 'w',
+        height: 'h',
+        minWidth: 'min-w',
+        minHeight: 'min-h',
+        maxWidth: 'max-w',
+        maxHeight: 'max-h',
+        padding: 'p',
+        margin: 'm',
+        boxShadow: 'shadows',
+        zIndex: 'z',
+        opacity: 'opacity',
+        gap: 'gap',
+        underline: 'underline',
+        textUnderlineOffset: 'underline-offset',
+      },
+      {
+        // options
+      }
+    )
   ]
 }
