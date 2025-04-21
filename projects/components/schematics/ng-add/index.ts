c59f81d7541593cf0ffe140b456b314bfe8e3138
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-04-21 22:30:36 +0200

    add onboarding page

--- projects/components/schematics/ng-add/index.ts
+++ projects/components/schematics/ng-add/index.ts
@@ -5,7 +5,7 @@ import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schema
 import { Schema } from './schema';
 
 const DEPENDENCIES = {
-  '@elementar-ui/components': '^0.2.24',
+  '@elementar-ui/components': '^0.2.25',
   "@dicebear/collection": "^9.2.2",
   "@dicebear/core": "^9.2.2",
   "@iconify-json/circle-flags": "^1.2.4",
